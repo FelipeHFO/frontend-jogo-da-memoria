08160da6e09feb33c80940fec5d5919cc74f9e8b
Felipe Oliveira <felipe.henrique.f.o@gmail.com>
2022-05-16 15:09:49 -0300

    feat: userequest criado

---
+++ hooks/useRequest.tsx
@@ -0,0 +1,33 @@
+export default function useRequest() {
+  const baseUrl = process.env.NEXT_PUBLIC_API_URL;
+
+  const sendGet = async (route: string, token?: string) =>
+    fetch(`${baseUrl}/${route}`, {
+      method: 'get',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+  const sendPost = async (route: string, data: unknown, token?: string) =>
+    fetch(`${baseUrl}/${route}`, {
+      method: 'post',
+      body: JSON.stringify(data),
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json',
+      },
+    });
+
+  const sendDelete = async (route: string, data: unknown, token?: string) =>
+    fetch(`${baseUrl}/${route}`, {
+      method: 'delete',
+      body: JSON.stringify(data),
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json',
+      },
+    });
+
+  return { sendGet, sendPost, sendDelete };
+}
