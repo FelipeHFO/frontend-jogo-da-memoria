ad9e0ede1db5099ccc200ff51522a562c02aaee8
Felipe Oliveira <felipe.henrique.f.o@gmail.com>
2022-05-31 17:42:34 -0300

    feat: Criado o hook useAuth

---
+++ hooks/useAuth.tsx
@@ -0,0 +1,7 @@
+import { useContext } from 'react';
+import { AuthContext } from 'contexts/Auth.context';
+
+export function useAuth() {
+  const context = useContext(AuthContext);
+  return context;
+}
