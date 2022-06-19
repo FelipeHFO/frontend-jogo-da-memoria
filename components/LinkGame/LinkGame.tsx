994ae9b8b2e8a4852cb179307afd1de57c11e055
FelipeHFO <felipe.henrique.f.o@gmail.com>
2022-06-18 21:06:45 -0300

    fix: ajuste no estilo dos games

--- components/LinkGame/LinkGame.tsx
+++ components/LinkGame/LinkGame.tsx
@@ -1,18 +1,19 @@
 import Link from 'next/link';
+import styles from './LinkGame.module.css';
 
 type LinkGameProps = {
-    link: string;
-    title: string;
-}
+  link: string;
+  title: string;
+};
 
-const LinkGame = ({ link, title}: LinkGameProps) => {
-    return (
-        <Link href={link}>
-            <div className="flex justify-center items-center w-96 h-96 bg-dark-blue hover:bg-blue-700 hover:cursor-pointer hover:select-none rounded-2xl m-5 shadow-xl">
-                <h1 className="text-3xl text-center">{title}</h1>
-            </div>
-        </Link>
-    )
-}
+const LinkGame = ({ link, title }: LinkGameProps) => {
+  return (
+    <Link href={link}>
+      <div className={styles.link}>
+        <h1 className={styles.gameTitle}>{title}</h1>
+      </div>
+    </Link>
+  );
+};
 
-export default LinkGame
+export default LinkGame;
--- pages/games/index.tsx
+++ pages/games/index.tsx
@@ -6,7 +6,7 @@ const Games = () => {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Games</h1>
-      <div className="flex justify-around items-center flex-wrap w-full p-20">
+      <div className={styles.linkGamesContainer}>
         <LinkGame link="/games/jogo-da-memoria" title="Jogo da Memória" />
         <LinkGame link="" title="Jogo da ..." />
         <LinkGame link="" title="Jogo da ..." />
