aff344f25847b161cf7b3105b7307aacbf12f81a
felipehfo <felipe.henrique.f.o@gmail.com>
2022-07-09 16:21:03 -0300

    tentar descobrir uma forma de atualizar os dados

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -1,15 +1,46 @@
 import Image from 'next/image';
+import { useState } from 'react';
 import styles from './Card.module.css';
+import { cards, turnAllCardsDown } from '../../components/Card/dados';
 
 type CardProps = {
-  imageCard: string;
-  altCard: string;
+  id: number;
+  guess: any[];
 };
 
-const Card = ({ imageCard, altCard }: CardProps) => {
+const Card = ({ id, guess }: CardProps) => {
+  const [currentCardId, setCurrentCardId] = useState(id);
+  const currentCard = cards.find((card) => card.id === currentCardId);
+  const [visible, setVisible] = useState(currentCard.isTurned);
+
+  function checkGuess(){
+    guess.length > 1 ? null : guess.push(id);
+    cards.map((card) => card.id === id ? card.isTurned = true : null);
+    console.log(cards);    
+    console.log(guess);
+    
+  }
+
+  function handleClick(){
+    !currentCard.wasDiscovered && !(guess.length > 1)  ? setVisible(!visible) : turnAllCardsDown();
+    checkGuess();
+  }
+
   return (
     <div className={styles.cards}>
-      <Image src={imageCard} alt={altCard} width={160} height={200} />
+      {
+        visible ?
+        (
+          <button type='button' className={styles.front} onClick={handleClick}>
+            <Image src={currentCard.frontImage} alt={currentCard.altImage} width={160} height={200}  />
+          </button>
+        ) : 
+        (
+          <button type='button' className={styles.back} onClick={handleClick}>
+            <Image src="/assets/card-images/background.jpg" alt="back" width={160} height={200} />
+          </button>
+        )
+      }
     </div>
   );
 };
--- pages/games/jogo-da-memoria.tsx
+++ pages/games/jogo-da-memoria.tsx
@@ -1,64 +1,28 @@
 import styles from './jogo-da-memoria.module.css';
 import Header from 'components/Header/Header';
-import Image from 'next/image';
 import Card from 'components/Card/Card';
-import { useId } from 'react';
+import { cards } from '../../components/Card/dados';
+import { useEffect, useState } from 'react';
 
 const JogoDaMemoria = () => {
-  const images = [
-    '/assets/card-images/aatrox.jpg',
-    '/assets/card-images/ahri.jpg',
-    '/assets/card-images/azir.jpg',
-    '/assets/card-images/diana.jpg',
-    '/assets/card-images/fiora.jpg',
-    '/assets/card-images/gragas.jpg',
-    '/assets/card-images/jhin.jpg',
-    '/assets/card-images/jinx.jpg',
-    '/assets/card-images/leesin.jpg',
-    // '/assets/card-images/mordekaiser.jpg',
-    // '/assets/card-images/nasus.jpg',
-    // '/assets/card-images/nocturne.jpg',
-    // '/assets/card-images/pantheon.jpg',
-    // '/assets/card-images/rammus.jpg',
-    // '/assets/card-images/taliyah.jpg',
-    // '/assets/card-images/teemo.jpg',
-    // '/assets/card-images/tryndamere.jpg',
-    // '/assets/card-images/twistedfate.jpg',
-    // '/assets/card-images/twitch.jpg',
-    // '/assets/card-images/vex.jpg',
-    // '/assets/card-images/viego.jpg',
-    // '/assets/card-images/vladimir.jpg',
-    // '/assets/card-images/warwick.jpg',
-    // '/assets/card-images/wukong.jpg',
-    // '/assets/card-images/yasuo.jpg',
-    // '/assets/card-images/zac.jpg',
-    // '/assets/card-images/zeri.jpg',
-    // '/assets/card-images/ziggs.jpg',
-    // '/assets/card-images/zilean.jpg',
-    // '/assets/card-images/zoe.jpg',
-  ];
-  const duplicatedImages = [
-    '/assets/card-images/aatrox.jpg',
-    '/assets/card-images/ahri.jpg',
-    '/assets/card-images/azir.jpg',
-    '/assets/card-images/diana.jpg',
-    '/assets/card-images/fiora.jpg',
-    '/assets/card-images/gragas.jpg',
-    '/assets/card-images/jhin.jpg',
-    '/assets/card-images/jinx.jpg',
-    '/assets/card-images/leesin.jpg',
-    ...images,
-  ];
+  const [cardsState, setCardsState] = useState([]);
+  const guess = [];
 
-  duplicatedImages.sort(() => Math.random() - 0.5);
+  useEffect(() => {
+    setCardsState(cards);
+  }, [cards]);
 
   return (
     <div className={styles.container}>
       <Header title="Jogo da Memória" />
 
       <main className={styles.main}>
-        {duplicatedImages.map((item, index) => (
-          <Card key={index} imageCard={item} altCard={item} />
+        {cardsState.map((item) => (
+          <Card 
+            key={item.id}
+            id={item.id}
+            guess={guess}
+          />
         ))}
       </main>
 
