35d0f7fb0c5791b3688e880c69f164f84101a2de
FelipeHFO <felipe.henrique.f.o@gmail.com>
2022-07-31 17:41:34 -0300

    feat: adicionado as funções nos botões do componente scores

--- components/Scores/Scores.tsx
+++ components/Scores/Scores.tsx
@@ -1,17 +1,30 @@
 import { useJogoDaMemoria } from 'hooks/useJogoDaMemoria';
+import { useRouter } from 'next/router';
 import styles from './Scores.module.css';
 
 const Scores = () => {
   const { points, numberOfPlays, numberOfErrors } = useJogoDaMemoria();
+  const router = useRouter();
 
   return (
-    <div className={styles.scores}>
-      <h1>Pontos: {points}</h1>
-      <h1>Número de Jogadas: {numberOfPlays}</h1>
-      <h1>Erros: {numberOfErrors}</h1>
+    <div className={styles.container}>
+      <div className={styles.scores}>
+        <h1>Pontos: {points}</h1>
+        <h1>Número de Jogadas: {numberOfPlays}</h1>
+        <h1>Erros: {numberOfErrors}</h1>
 
-      <button type="button">Jogar novamente</button>
-      <button type="button">Sair</button>
+        <button type="button" onClick={() => window.location.reload()}>
+          Jogar novamente
+        </button>
+        <button
+          type="button"
+          onClick={() => {
+            router.push('/games');
+          }}
+        >
+          Sair
+        </button>
+      </div>
     </div>
   );
 };
